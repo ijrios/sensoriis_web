35ce1b826b261e28dbf849080237ef914177bd0d
Jose Rios <joserios055@gmail.com>
2022-04-05 20:22:33 -0500

    This is a new commit for what I originally planned to be amended

--- view/src/App.js
+++ view/src/App.js
@@ -20,29 +20,35 @@ const App = () => {
   }
   
   return(
-<center>
-<br />
-<div className="title">
-<h1> Estado de los sensores en tiempo real</h1>   
-</div>
-
-<div className="app mx-auto App-container">
-<div className="app-body">
-    <div className="row center">
-        <div className="col-6 App-Info">
-        <ul>
-         {
-            dato.map(item => (
-              <p key ="item.id">{item.title} - {item.value}</p>
-            ))
-         }
-       </ul>
-     </div>
+  <center>
+    <br />
+    <br />
+    <br />
+    <br />
+    <div className="title">
+    <h1> Estado de los sensores en tiempo real</h1>   
     </div>
-</div>
-</div>
-</center>
+
+    <div className="app mx-auto App-container">
+      <div className="app-body">
+       <div className="row center">
+          <div className="col-6 App-Info">
+            <ul>
+               <br />
+                  {
+                  dato.map(item => (
+                  <p key ="item.id">{item.title}: {item.value}</p>
+              
+                  ))
+                  }
+                <br />
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </center>
   )
-}
+  }
 
 export default App;
