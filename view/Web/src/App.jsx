5a663bb408d5a1147e4a6d473bb994780bb4e5d8
Jose Rios <joserios055@gmail.com>
2022-04-17 09:26:43 -0500

    This is a new commit for what I originally planned to be amended

--- view/Web/src/App.jsx
+++ view/Web/src/App.jsx
@@ -14,20 +14,26 @@ const AppContainer = tw.div`
   pb-10
   pl-10
   pr-10
+  
 `;
 
 const Title = tw.h1`
   text-4xl
   font-extrabold
+  shadow-lg
 `;
 
 function App() {
   return (
+    
     <AppContainer>
       <br/><br/><br/><br/><br/><br/>
       <Title>S E N S O R E S</Title>
       <Sensores />
-    </AppContainer>
+      <br/><br/>
+      <p>Esta aplicación muestra el estado real de los sensores enviados desde Raspberry Pi</p>
+     </AppContainer>
+    
   );
 }
 
